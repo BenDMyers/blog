b6e5a69840da0fce70a05f8062a049a58a3383ad
Ben Myers <ben.myers@okstate.edu>
2019-09-01 10:20:56 -0500

    Revised Domino's Part 1 based on feedback

--- src/components/post/Footnotes/FootnoteReference.js
+++ src/components/post/Footnotes/FootnoteReference.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const FootnoteReference = ({fn}) => {
-	let screenreaderText = `Go to footnote ${fn}`;
 	return (
 		<a href={`#fn-${fn}`} id={`fn-ref-${fn}`}>
-			<span className="screenreader">{screenreaderText}</span>
-			<sup aria-hidden="true">{fn}</sup>
+			<span className="screenreader">Go to footnote </span>
+			<sup>{fn}</sup>
 		</a>
 	);
 };

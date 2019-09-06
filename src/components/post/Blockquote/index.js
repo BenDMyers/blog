b723d53fecee419020e756bf2fe8f35f6035fdd9
Ben Myers <ben.myers@okstate.edu>
2019-09-05 20:34:57 -0500

    Blockquote semantics

--- src/components/post/Blockquote/index.js
+++ src/components/post/Blockquote/index.js
@@ -2,20 +2,14 @@ import React from 'react';
 import './blockquote.css';
 
 const Blockquote = (props) => {
-	let quote = (
-		<blockquote className="quote-contents">{props.children}</blockquote>
+	return (
+		<blockquote>
+			<p className="quote-contents">{props.children}</p>
+			{props.attribution && (
+				<cite className="quote-attribution">{props.attribution}</cite>
+			)}
+		</blockquote>
 	);
-
-	if (props.attribution) {
-		quote = (
-			<figure className="quote-figure">
-				{quote}
-				<cite className="quote-attribution">—{props.attribution}</cite>
-			</figure>
-		);
-	}
-
-	return quote;
 };
 
 export default Blockquote;

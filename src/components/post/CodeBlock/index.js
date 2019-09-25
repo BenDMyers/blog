67299c2d30203addebe9608280ae78dc990c6382
Ben Myers <ben.myers@okstate.edu>
2019-09-25 12:48:39 -0500

    Enabled line numbers and highlighting in codeblocks

--- src/components/post/CodeBlock/index.js
+++ src/components/post/CodeBlock/index.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import Highlight, {defaultProps} from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/oceanicNext';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 
 import './code-block.css';
+
+const HIGHLIGHT_RANGE_REGEX = /{([\d,-]+)}/;
+
+const calculateLinesToHighlight = (meta) => {
+	if (HIGHLIGHT_RANGE_REGEX.test(meta)) {
+		const lineNumbers = HIGHLIGHT_RANGE_REGEX.exec(meta)[1]
+			.split(',')
+			.map((v) => v.split('-').map((y) => parseInt(y, 10)));
+		return (index) => {
+			const lineNumber = index + 1;
+			const inRange = lineNumbers.some(([start, end]) =>
+				end
+					? lineNumber >= start && lineNumber <= end
+					: lineNumber === start
+			);
+			return inRange;
+		};
+	} else {
+		return () => false;
+	}
+};
+
+const processProps = (props) => {
+	const {children, className, metastring} = props.children.props;
+	const language = className.replace(/language-/, '');
+
+	const hlProps = {...defaultProps, code: children, language, theme};
+	const showLineNumbers = metastring.includes('{numberLines: true}');
+	const shouldHighlightLine = calculateLinesToHighlight(metastring);
+
+	return [hlProps, showLineNumbers, shouldHighlightLine];
+};
 
 /**
  * Displays a syntax-highlighted block of code
  * @link https://mdxjs.com/guides/syntax-highlighting
  */
 const CodeBlock = (props) => {
-	console.log(props, defaultProps);
-	const {children, className, metastring} = props.children.props;
-	const language = className.replace(/language-/, '');
-	const showLineNumbers = metastring.includes('numberLines');
+	const [hlProps, showLineNumbers, shouldHighlightLine] = processProps(props);
+
 	return (
-		<Highlight
-			{...defaultProps}
-			code={children}
-			language={language}
-			theme={theme}
-		>
+		<Highlight {...hlProps}>
 			{({className, tokens, getLineProps, getTokenProps}) => (
 				<pre className={`codeblock ${className}`}>
 					{tokens.map((line, i) => {
@@ -29,7 +53,24 @@ const CodeBlock = (props) => {
 						}
 
 						return (
-							<div key={i} {...getLineProps({line, key: i})}>
+							<div
+								key={i}
+								{...getLineProps({
+									line,
+									key: i,
+									className: shouldHighlightLine(i)
+										? 'highlight-line'
+										: ''
+								})}
+							>
+								{showLineNumbers && (
+									<span className="line-number">
+										<span className="screenreader">
+											Line&nbsp;
+										</span>
+										{i + 1}&nbsp;
+									</span>
+								)}
 								{line.map((token, key) => (
 									<span
 										key={key}

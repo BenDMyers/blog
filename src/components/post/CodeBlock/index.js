cfc784214160b4a4bc449ab2e34b1a51b7ed9f7d
Ben Myers <ben.myers@okstate.edu>
2019-09-25 14:05:32 -0500

    Improved codeblock dimensions and scrolling

--- src/components/post/CodeBlock/index.js
+++ src/components/post/CodeBlock/index.js
@@ -3,6 +3,8 @@ import Highlight, {defaultProps} from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/oceanicNext';
 
 import './code-block.css';
+import {rhythm} from '../../../utils/typography';
+import {useScrollDimensions} from '../../../utils/use-dimensions';
 
 const HIGHLIGHT_RANGE_REGEX = /{([\d,-]+)}/;
 
@@ -41,13 +43,25 @@ const processProps = (props) => {
  * @link https://mdxjs.com/guides/syntax-highlighting
  */
 const CodeBlock = (props) => {
+	const [preRef, preDimensions] = useScrollDimensions();
 	const [hlProps, showLineNumbers, shouldHighlightLine] = processProps(props);
+	const preStyle = {
+		width: `calc(100% + ${rhythm(3 / 4)} + ${rhythm(3 / 4)})`,
+		marginLeft: `-${rhythm(3 / 4)}`
+	};
 
 	return (
 		<Highlight {...hlProps}>
 			{({className, tokens, getLineProps, getTokenProps}) => (
-				<pre className={`codeblock ${className}`}>
+				/* CREATE CODEBLOCK */
+				<pre
+					ref={preRef}
+					style={preStyle}
+					className={`codeblock ${className}`}
+				>
+					{/* CREATE LINES */}
 					{tokens.map((line, i) => {
+						// SKIP FINAL, EMPTY LINE
 						if (i === tokens.length - 1 && line[0].empty) {
 							return null;
 						}
@@ -60,9 +74,13 @@ const CodeBlock = (props) => {
 									key: i,
 									className: shouldHighlightLine(i)
 										? 'highlight-line'
-										: ''
+										: '',
+									style: {
+										width: `${preDimensions.width}px`
+									}
 								})}
 							>
+								{/* RENDER LINE NUMBERS IF APPLICABLE */}
 								{showLineNumbers && (
 									<span className="line-number">
 										<span className="screenreader">
@@ -71,6 +89,10 @@ const CodeBlock = (props) => {
 										{i + 1}&nbsp;
 									</span>
 								)}
+								{!showLineNumbers && (
+									<span className="no-line-numbers" />
+								)}
+								{/* LINE CONTENTS */}
 								{line.map((token, key) => (
 									<span
 										key={key}

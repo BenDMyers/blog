f4240ebe4d52e43ca777b2b47e98fa2d222f9114
Ben Myers <ben.myers@okstate.edu>
2020-02-09 16:00:18 -0600

    Pretty much the whole post. Whoops.

---
+++ src/examples/css-impacts-screenreaders/EditButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './edit-button.css';
+
+const EditButton = () => {
+    return (
+        <button className="edit">
+            Edit
+        </button>
+    );
+}
+
+export default EditButton;

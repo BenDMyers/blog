ad1f0cd67bce0d9be74be41c64bd9486174246f6
Ben Myers <ben.myers@okstate.edu>
2020-02-10 21:20:36 -0600

    Side-by-siding snippets

---
+++ src/examples/css-impacts-screenreaders/listItemsDemo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './demos.css';
+
+const ListItemsDemo = () => {
+    return (
+        <div className="demo">
+            <ul>
+                <li>Apples</li>
+                <li>Bananas</li>
+            </ul>
+        </div>
+    );
+}
+
+export default ListItemsDemo;

9760b9dcdf5cc832eb12b137f5a577ec3d40a60d
Ben Myers <ben.myers@okstate.edu>
2020-02-15 17:03:24 -0600

    Start scope post

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,4 +5,5 @@ import 'typeface-merriweather';
 import Prism from "prism-react-renderer/prism";
 (typeof global !== "undefined" ? global : window).Prism = Prism;
 
+require("prismjs/components/prism-bash");
 require("prismjs/components/prism-batch");
